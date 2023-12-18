a0b3f9040dd6f72be3470bfcd1465cf90e0811ca
j_k_b <j_k_b@BLUEEYESYSTEMS>
2023-12-18 12:13:19 +0100

    Add project files.

---
+++ ManagementCentral/Client/Components/DeviceList.razor.cs
@@ -0,0 +1,28 @@
+﻿using ManagementCentral.Client.Services;
+using ManagementCentral.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace ManagementCentral.Client.Components
+{
+ 
+
+    public partial class DeviceList
+    {
+        [Parameter]
+        public string ExtraCaption { get; set; } = string.Empty;
+
+        [Inject]
+        public IDeviceDataService? DeviceDataService { get; set; }
+
+        public List<Device> DeviceLst { get; set; } = new List<Device>();
+
+        protected override void OnInitialized()
+        {
+
+            DeviceLst = DeviceDataService.GetDevices();
+            base.OnInitialized();
+
+        }
+
+    }
+}
---
+++ ManagementCentral/Shared/Domain/Device.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementCentral.Shared.Domain
+{
+    public enum Status
+    {
+        online,
+        offline
+    }
+
+    public enum Location
+    {
+        Sweden,
+        England
+    }
+    public class Device
+    {
+        public int DeviceId { get; set; }
+
+        public Location Location { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string DeviceType { get; set; } = string.Empty;
+
+        public Status Status { get; set; } 
+    }
+}
