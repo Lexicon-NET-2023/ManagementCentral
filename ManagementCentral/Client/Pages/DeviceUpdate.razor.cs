dd7e573fd81a38e9af992f00941b5ef803eb608c
Jonas Berg <j_k_berg@hotmail.com>
2023-12-18 15:08:14 +0100

    DeviceUpdate functions.

---
+++ ManagementCentral/Client/Pages/DeviceUpdate.razor.cs
@@ -0,0 +1,34 @@
+﻿using ManagementCentral.Client.Services;
+using ManagementCentral.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace ManagementCentral.Client.Pages
+{
+    public partial class DeviceUpdate
+    {
+
+        [Inject]
+        public IDeviceDataService DeviceDataService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string DeviceId { get; set; } = string.Empty;
+
+        public Device Device { get; set; } = new Device();
+
+        protected override void OnInitialized()
+        {
+            Device = DeviceDataService.GetDevice(int.Parse(DeviceId));
+
+            base.OnInitialized();
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            DeviceDataService.UpdateDevice(Device);
+            NavigationManager.NavigateTo($"listofdevices");
+        }
+    }
+}
