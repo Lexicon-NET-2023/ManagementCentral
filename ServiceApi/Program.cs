3f403a2e0836be92a8cd8d95901c60d8f9319032
Jonas Berg <j_k_berg@hotmail.com>
2023-12-19 15:17:00 +0100

    Updated APIRequest and added new endpoints in API

--- ServiceApi/Program.cs
+++ ServiceApi/Program.cs
@@ -1,3 +1,4 @@
+using ManagementCentral.Shared.Domain;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -56,10 +57,27 @@ app.MapGet("/device", () => "Getting a device from API");
 app.MapGet("/device/{deviceId}/button/{buttonId}",
     (int deviceId, int buttonId) => $"Deviceid {deviceId} and ButtonId {buttonId}");
 
+app.MapGet("/device/{deviceId}", (int deviceId) =>
+{
+    var DeviceService = new DeviceService();
+    return DeviceService.DeviceList[deviceId].DeviceType;
+});
+
 app.UseCors(specOrigin);
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+class DeviceService
+{
+    public List<Device> DeviceList { get; set; } = new List<Device>();
+
+    public DeviceService()
+    {
+        DeviceList.Add(new Device() { DeviceId = 1, Location = Location.Sweden, Date = DateTime.Now, DeviceType = "Sensor", Status = Status.online });
+        DeviceList.Add(new Device() { DeviceId = 2, Location = Location.England, Date = DateTime.Now.AddDays(-30), DeviceType = "Machine", Status = Status.offline });
+    }
 }
