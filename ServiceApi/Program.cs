9e2f3aa8e0c2de4e32913c561893d6163afe2631
Jonas Berg <j_k_berg@hotmail.com>
2023-12-20 12:00:44 +0100

    API added. Program.cs file updated and endpoints moved in to class.

--- ServiceApi/Program.cs
+++ ServiceApi/Program.cs
@@ -1,3 +1,4 @@
+using DeviceApi.Endpoints;
 using ManagementCentral.Shared.Domain;
 using Microsoft.Extensions.Options;
 
@@ -31,53 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-// device
-app.MapGet("/device", () => "Getting a device from API");
-
-app.MapGet("/device/{deviceId}/button/{buttonId}",
-    (int deviceId, int buttonId) => $"Deviceid {deviceId} and ButtonId {buttonId}");
-
-app.MapGet("/device/{deviceId}", (int deviceId) =>
-{
-    var DeviceService = new DeviceService();
-    return DeviceService.DeviceList[deviceId].DeviceType;
-});
+app.RegisterUserEndpoint();
 
 app.UseCors(specOrigin);
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-class DeviceService
-{
-    public List<Device> DeviceList { get; set; } = new List<Device>();
-
-    public DeviceService()
-    {
-        DeviceList.Add(new Device() { DeviceId = 1, Location = Location.Sweden, Date = DateTime.Now, DeviceType = "Sensor", Status = Status.online });
-        DeviceList.Add(new Device() { DeviceId = 2, Location = Location.England, Date = DateTime.Now.AddDays(-30), DeviceType = "Machine", Status = Status.offline });
-    }
-}
