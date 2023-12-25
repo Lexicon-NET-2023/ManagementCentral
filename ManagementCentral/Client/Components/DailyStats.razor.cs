c4f542493a8528a1f94a96f086f3df15bedfe12b
Jonas Berg <j_k_berg@hotmail.com>
2023-12-25 17:12:49 +0100

    Updated DailyStats to get information from API Updated left Menu and removed FetchData and API Requests (this was just tests)

--- ManagementCentral/Client/Components/DailyStats.razor.cs
+++ ManagementCentral/Client/Components/DailyStats.razor.cs
@@ -1,21 +1,55 @@
-﻿using ManagementCentral.Client.Services;
-using Microsoft.AspNetCore.Components;
+﻿using ManagementCentral.Shared.Domain;
+using System.Text.Json;
 
 namespace ManagementCentral.Client.Components
 {
     public partial class DailyStats
     {
-        [Inject]
-        public IDeviceDataService DeviceDataService { get; set; }
+        public List<Device> DeviceList { get; set; } = new List<Device>();
 
-        public int NumberOfDevice {  get; set; }    
+        public string responseData = string.Empty;
 
-        protected override void OnInitialized()
+        public string ErrorMessage = string.Empty;
+
+        public int NumberOfDevice { get; set; }
+
+        protected override async Task OnInitializedAsync()
         {
-            NumberOfDevice = DeviceDataService.GetDevices().Count;
+            //var authState = await authenticationStateTask;
+            //if (!authState.User.IsInRole("Admin")) 
+            //{
+            //    return;
+            //}
 
-            base.OnInitialized();
+            try
+            {
+                var response = await Http.GetAsync("/devices");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    responseData = await response.Content.ReadAsStringAsync();
+
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                        PropertyNameCaseInsensitive = true,
+                    };
+
+                    DeviceList = (List<Device>)JsonSerializer.Deserialize<IEnumerable<Device>>(responseData, options)!;
+                    NumberOfDevice = DeviceList.Count;
+                }
+                else
+                {
+                    ErrorMessage = "Could not get data from API! " + response.StatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+
+            await base.OnInitializedAsync();
+
         }
-
     }
 }
